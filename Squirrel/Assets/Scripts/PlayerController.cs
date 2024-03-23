e6efe3e442a3bd411d2c28881bc6854272853c22
Benlox44 <benjagilberto44@gmail.com>
2024-03-23 05:17:25 -0300

    primer commit

---
+++ Squirrel/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    [SerializeField]
+    private float speed;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed;
+
+        animator.SetFloat("moveX", rigidBody.velocity.x);
+        animator.SetFloat("moveY", rigidBody.velocity.y);
+        
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
