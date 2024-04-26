d32e7bb167dd4409382c2792bc32241fbe28c586
Benlox44 <benjagilberto44@gmail.com>
2024-04-26 16:35:29 -0400

    ol

--- Squirrel/Assets/Scripts/CameraController.cs
+++ Squirrel/Assets/Scripts/CameraController.cs
@@ -9,11 +9,16 @@ public class CameraController : MonoBehaviour
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
+    [SerializeField] private Vector2 minPosition;
+    [SerializeField] private Vector2 maxPosition;
+
     [SerializeField] private Transform target;
 
     void Update()
     {
         Vector3 targetPosition = target.position + offset;
+        targetPosition.x = Mathf.Clamp(target.position.x, minPosition.x, maxPosition.x);
+        targetPosition.y = Mathf.Clamp(target.position.y, minPosition.y, maxPosition.y);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime); 
     }
 }
