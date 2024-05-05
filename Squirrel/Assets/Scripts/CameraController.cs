c3b3c3dd3c6a94ac5950723fb9327aed02430e0f
Benlox44 <benjagilberto44@gmail.com>
2024-05-04 20:04:44 -0400

    Area transition 1
    mover limite camara derecha y avanze del mapa de la derecha del lvl 1

--- Squirrel/Assets/Scripts/CameraController.cs
+++ Squirrel/Assets/Scripts/CameraController.cs
@@ -1,21 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
 
     private Vector3 offset = new Vector3(0f, 0f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
-    [SerializeField] private Vector2 minPosition;
-    [SerializeField] private Vector2 maxPosition;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     [SerializeField] private Transform target;
 
-    void Update()
-    {
+    void Update() {
         Vector3 targetPosition = target.position + offset;
         targetPosition.x = Mathf.Clamp(target.position.x, minPosition.x, maxPosition.x);
         targetPosition.y = Mathf.Clamp(target.position.y, minPosition.y, maxPosition.y);
